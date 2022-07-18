1df54fb2639b0607f5b6f297375ac11a0d07b4b9
김송이 <ksl2950@naver.com>
2022-07-18 21:48:02 +0900

    [feat] User Entity, Repository, Service 구현(#4)

---
+++ src/main/java/com/cakestation/backend/domain/Role.java
@@ -0,0 +1,5 @@
+package com.cakestation.backend.domain;
+
+public enum Role {
+    ROLE_USER, ROLE_GUEST, ROLE_ADMIN
+}
---
+++ src/main/java/com/cakestation/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cakestation.backend.repository;
+
+import com.cakestation.backend.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findUserByEmail(String email);
+
+}
