dd5a721b4647939f698d4519aa346ece30221b3a
Songyi Kim <ksl2950@naver.com>
2022-08-01 16:31:13 +0900

    [test] 리뷰 등록 서비스 단위 테스트 작성 (#16)

---
+++ src/test/java/com/cakestation/backend/review/service/ReviewServiceUnitTest.java
@@ -0,0 +1,58 @@
+package com.cakestation.backend.review.service;
+
+import com.cakestation.backend.review.domain.Review;
+import com.cakestation.backend.review.dto.request.CreateReviewDto;
+import com.cakestation.backend.review.fixture.ReviewFixture;
+import com.cakestation.backend.review.repository.ReviewRepository;
+import com.cakestation.backend.store.repository.StoreRepository;
+import com.cakestation.backend.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static com.cakestation.backend.review.fixture.ReviewFixture.REVIEW_ID;
+import static com.cakestation.backend.review.fixture.ReviewFixture.reviewEntity;
+import static com.cakestation.backend.store.fixture.StoreFixture.STORE_ID;
+import static com.cakestation.backend.store.fixture.StoreFixture.storeEntity;
+import static com.cakestation.backend.user.fixture.UserFixture.userEntity;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ReviewServiceUnitTest {
+
+    @Mock
+    UserRepository userRepository;
+    @Mock
+    StoreRepository storeRepository;
+    @Mock
+    ReviewRepository reviewRepository;
+
+    @InjectMocks
+    ReviewService reviewService;
+
+    @Test
+    void 리뷰_등록(){
+        // given
+        CreateReviewDto reviewDto = ReviewFixture.reviewDto();
+
+        doReturn(Optional.of(userEntity())).when(userRepository).findById(any());
+        doReturn(Optional.of(storeEntity())).when(storeRepository).findById(any());
+        doReturn(reviewEntity()).when(reviewRepository).save(any());
+
+        // when
+        Long reviewId = reviewService.saveReview(STORE_ID, reviewDto);
+
+        // then
+        assertEquals(reviewId,REVIEW_ID);
+
+    }
+}
---
+++ src/test/java/com/cakestation/backend/store/service/StoreServiceUnitTest.java
@@ -0,0 +1,13 @@
+package com.cakestation.backend.store.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class StoreServiceUnitTest {
+
+    @Test
+    void 가게_등록() {
+    }
+}
