edb0c7185b3a48512b0c4ea01eb09dfd71f72110
AlphaTechnic <alphatechnicist@gmail.com>
2022-08-18 09:01:03 +0900

    [feat] create dto StoreResponse

--- src/main/java/com/cakestation/backend/store/dto/response/StoreResponse.java
+++ src/main/java/com/cakestation/backend/store/dto/response/StoreResponse.java
@@ -1,14 +1,13 @@
 package com.cakestation.backend.store.dto.response;
 
 import com.cakestation.backend.store.domain.Store;
-import com.cakestation.backend.store.dto.request.CreateStoreDto;
 import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
 @Getter
-public class ShowStoreDto {
+public class StoreResponse {
     private String name;
 
     private String address;
@@ -25,15 +24,15 @@ public class ShowStoreDto {
 
     private Double score;
 
-    public static Store toEntity(ShowStoreDto showStoreDto){
-        return Store.builder()
-                .name(showStoreDto.getName())
-                .address(showStoreDto.getAddress())
-                .businessHours(showStoreDto.getBusinessHours())
-                .phone(showStoreDto.getPhone())
-                .photoUrl(showStoreDto.getPhotoUrl())
-                .webpageUrl(showStoreDto.getWebpageUrl())
-                .kakaoMapUrl(showStoreDto.getKakaoMapUrl())
+    public static StoreResponse from(Store store){
+        return StoreResponse.builder()
+                .name(store.getName())
+                .address(store.getAddress())
+                .businessHours(store.getBusinessHours())
+                .phone(store.getPhone())
+                .photoUrl(store.getPhotoUrl())
+                .webpageUrl(store.getWebpageUrl())
+                .kakaoMapUrl(store.getKakaoMapUrl())
                 .build();
     }
 }
