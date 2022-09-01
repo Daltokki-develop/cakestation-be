f0acbef95312bd1c5216118f72ff9b93b003d555
AlphaTechnic <alphatechnicist@gmail.com>
2022-09-01 20:34:58 +0900

    [feat] sotres/all 구현

--- src/main/java/com/cakestation/backend/store/service/StoreService.java
+++ src/main/java/com/cakestation/backend/store/service/StoreService.java
@@ -8,6 +8,8 @@ import com.cakestation.backend.store.repository.StoreRepository;
 import com.cakestation.backend.user.repository.UserRepository;
 import com.cakestation.backend.store.dto.request.CreateStoreDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,5 +36,9 @@ public class StoreService {
             System.out.println("error!");
         }
         return storeOptional.get();
+    }
+
+    public Page<Store> findAllStores(Pageable pageable) {
+        return storeRepository.findAll(pageable);
     }
 }
