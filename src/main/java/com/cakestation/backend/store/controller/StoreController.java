b3bc7474d6ba34f3eca3354f997da1ca2d753b0a
AlphaTechnic <58129950+AlphaTechnic@users.noreply.github.com>
2022-09-10 15:56:28 +0900

    [feat] sotres/all 구현 (#42)

--- src/main/java/com/cakestation/backend/store/controller/StoreController.java
+++ src/main/java/com/cakestation/backend/store/controller/StoreController.java
@@ -6,6 +6,10 @@ import com.cakestation.backend.store.dto.response.StoreResponse;
 import com.cakestation.backend.store.service.StoreService;
 import com.cakestation.backend.store.dto.request.CreateStoreDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -32,5 +36,14 @@ public class StoreController {
         Store store = storeService.findStoreById(storeId);
         StoreResponse response = StoreResponse.from(store);
         return ResponseEntity.ok().body(new ApiResponse<>(HttpStatus.OK.value(), "가게 조회 성공", response));
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/stores/all")
+    public ResponseEntity<ApiResponse<Page<StoreResponse>>> showAllStores(@PageableDefault(size = 10, sort = "uploadDate", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Store> storeList = storeService.findAllStores(pageable);
+        Page<StoreResponse> storeResponseList = storeList.map(StoreResponse::from);
+        return ResponseEntity.ok().body(
+                new ApiResponse<>(HttpStatus.OK.value(), "가게들 조회 성공", storeResponseList));
     }
 }
