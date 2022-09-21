dde69cf23592029ae34380ad7428680f46156ad6
junu0810 <junu0810@hanmail.net>
2022-09-21 10:26:59 +0900

    Filter를 통한 권한구현 (#59)

---
+++ src/main/java/com/cakestation/backend/common/Filter/Filter.java
@@ -0,0 +1,15 @@
+package com.cakestation.backend.common.Filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public interface Filter extends javax.servlet.Filter {
+
+    public default void init(FilterConfig filterConfig) throws ServletException {}
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException;
+
+    public default void destroy() {}
+
+}
