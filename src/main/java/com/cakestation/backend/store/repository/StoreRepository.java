f70779203d9ac719e42dadd5b2c01af6d9a70581
Songyi Kim <ksl2950@naver.com>
2022-10-05 23:24:14 +0900

    [refactor] 가게 조회 정보 수정 및 리팩토링

--- src/main/java/com/cakestation/backend/store/repository/StoreRepository.java
+++ src/main/java/com/cakestation/backend/store/repository/StoreRepository.java
@@ -9,29 +9,14 @@ import com.cakestation.backend.store.domain.Store;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store,Long> {
-    static <T> Page<T> createPageFromList(List<T> list, Pageable pageable) {
-        if (list == null) {
-            throw new IllegalArgumentException("To create a Page, the list mustn't be null!");
-        }
 
-        int startOfPage = pageable.getPageNumber() * pageable.getPageSize();
-        if (startOfPage > list.size()) {
-            return new PageImpl<>(new ArrayList<>(), pageable, 0);
-        }
+    List<Store> findAllByNameContains(@Param("storeName") String StoreName, Pageable pageable);
 
-        int endOfPage = Math.min(startOfPage + pageable.getPageSize(), list.size());
-        return new PageImpl<>(list.subList(startOfPage, endOfPage), pageable, list.size());
-    }
-    public default Page<Store> findStoresByStoreName(Pageable pageable, String StoreName) {
-        List<Store> storesFilteredByName = findAll().stream()
-                .filter(store -> store.getName().contains(String.format("%s", StoreName)))
-                .collect(Collectors.toList());
-
-        return createPageFromList(storesFilteredByName, pageable);
-    }
 }
