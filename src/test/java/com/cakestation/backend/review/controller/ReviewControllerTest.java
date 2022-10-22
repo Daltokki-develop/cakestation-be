48e485efe02d309c8282fd46615b49306ec39b5f
Songyi Kim <ksl2950@naver.com>
2022-10-22 11:38:42 +0900

    [test] 테스트 url 변경

--- src/test/java/com/cakestation/backend/review/controller/ReviewControllerTest.java
+++ src/test/java/com/cakestation/backend/review/controller/ReviewControllerTest.java
@@ -167,7 +167,7 @@ class ReviewControllerTest {
         CreateReviewDto createReviewDto = createReviewRequest.toServiceDto(STORE_ID, createReviewRequest);
         reviewService.saveReview(createReviewDto, getKakaoUserDto().getEmail());
 
-        String uri = String.format("/api/stores/%d/reviewImages", STORE_ID);
+        String uri = String.format("/api/stores/%d/reviews/image", STORE_ID);
         MvcResult result = mockMvc.perform(
                         MockMvcRequestBuilders.get(uri)
                                 .accept(MediaType.APPLICATION_JSON)
@@ -177,4 +177,28 @@ class ReviewControllerTest {
                 .andReturn();
     }
 
+    @Test
+    public void HTTP_리뷰이미지_조회_by_사용자() throws Exception {
+
+        //given
+        // 회원 등록
+        userService.join(getKakaoUserDto());
+
+        // 가게 등록
+        storeService.saveStore(getStoreDto());
+
+        // 리뷰 등록
+        CreateReviewRequest createReviewRequest = ReviewFixture.getCreateReviewRequest();
+        CreateReviewDto createReviewDto = createReviewRequest.toServiceDto(STORE_ID, createReviewRequest);
+        reviewService.saveReview(createReviewDto, getKakaoUserDto().getEmail());
+
+        String uri = String.format("/api/users/%d/reviews/image", USER_ID);
+        MvcResult result = mockMvc.perform(
+                        MockMvcRequestBuilders.get(uri)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
 }
