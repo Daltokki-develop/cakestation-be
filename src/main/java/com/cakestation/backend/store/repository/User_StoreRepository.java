f96b4e014d5ff29a5160dbede163a07ebf6cafb4
junu0810 <junu0810@hanmail.net>
2022-10-23 18:40:31 +0900

    fix Filename

--- src/main/java/com/cakestation/backend/store/repository/User_StoreRepository.java
+++ src/main/java/com/cakestation/backend/store/repository/User_StoreRepository.java
@@ -1,10 +1,9 @@
 package com.cakestation.backend.store.repository;
 
-
-import com.cakestation.backend.store.domain.Store;
+import com.cakestation.backend.store.domain.User_Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface User_StoreRepository extends JpaRepository<Store,Long> {
+public interface User_StoreRepository extends JpaRepository<User_Store,Long> {
 }
