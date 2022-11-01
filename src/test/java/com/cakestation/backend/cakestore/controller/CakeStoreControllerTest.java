d63d2f69c4f3ffd523892812c51d19f52210fffe
Songyi Kim <ksl2950@naver.com>
2022-11-01 09:46:25 +0900

    [fix] 통합 테스트 DB 변경

--- src/test/java/com/cakestation/backend/cakestore/controller/CakeStoreControllerTest.java
+++ src/test/java/com/cakestation/backend/cakestore/controller/CakeStoreControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -19,10 +20,10 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import static com.cakestation.backend.cakestore.fixture.StoreFixture.getCreateCakeStoreDto;
 import static com.cakestation.backend.user.fixture.UserFixture.getKakaoUserDto;
 
-@Testcontainers
 @AutoConfigureMockMvc
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@TestPropertySource(properties = {"spring.config.location=classpath:application-test.yml"})
 public class CakeStoreControllerTest {
 
     @Autowired
