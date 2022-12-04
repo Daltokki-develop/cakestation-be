8b3b770575d71c0f845ed35b87eeb34df435d49e
Songyi Kim <ksl2950@naver.com>
2022-12-04 21:05:06 +0900

    [feat] 리뷰 수정 dto 추가

---
+++ src/main/java/com/cakestation/backend/review/controller/dto/request/UpdateReviewRequest.java
@@ -0,0 +1,40 @@
+package com.cakestation.backend.review.controller.dto.request;
+
+import com.cakestation.backend.review.domain.DesignSatisfaction;
+import com.cakestation.backend.review.domain.Tag;
+import com.cakestation.backend.review.service.dto.UpdateReviewDto;
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateReviewRequest {
+    private List<MultipartFile> reviewImages = new ArrayList<>(); // 리뷰 이미지 파일 리스트
+    private int cakeNumber; // 케이크 호수
+    private String sheetType; // 시트 종류
+    private String requestOption; // 추가 옵션
+    private DesignSatisfaction designSatisfaction; // 만족도
+    private int score; // 별점
+    private List<Tag> tags;
+    private String content; // 하고 싶은 말
+
+    public UpdateReviewDto toServiceDto(Long storeId, UpdateReviewRequest updateReviewRequest) {
+        return UpdateReviewDto.builder()
+                .storeId(storeId)
+                .reviewImages(updateReviewRequest.getReviewImages())
+                .cakeNumber(updateReviewRequest.getCakeNumber())
+                .sheetType(updateReviewRequest.getSheetType())
+                .requestOption(updateReviewRequest.getRequestOption())
+                .designSatisfaction(updateReviewRequest.getDesignSatisfaction())
+                .score(updateReviewRequest.getScore())
+                .tags(updateReviewRequest.getTags())
+                .content(updateReviewRequest.getContent())
+                .build();
+    }
+}
