11ffa35928d9f9005610d8095b163657d0871461
Songyi Kim <ksl2950@naver.com>
2023-02-01 17:22:09 +0900

    [test] static method mocking 처리

--- src/test/java/com/cakestation/backend/review/controller/ReviewControllerTest.java
+++ src/test/java/com/cakestation/backend/review/controller/ReviewControllerTest.java
@@ -3,14 +3,18 @@ package com.cakestation.backend.review.controller;
 
 import com.cakestation.backend.common.ApiResponse;
 import com.cakestation.backend.common.ControllerTest;
+import com.cakestation.backend.common.UtilService;
 import com.cakestation.backend.review.controller.dto.request.UpdateReviewRequest;
 import com.cakestation.backend.review.controller.dto.response.ReviewResponse;
 import com.cakestation.backend.review.domain.Tag;
 import com.cakestation.backend.review.service.dto.CreateReviewDto;
 import com.cakestation.backend.review.service.dto.ReviewDto;
 import com.cakestation.backend.review.service.dto.UpdateReviewDto;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,11 +23,13 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import java.util.List;
 
 import static com.cakestation.backend.cakestore.fixture.StoreFixture.STORE_ID_1;
+import static com.cakestation.backend.common.UtilService.getCurrentUserEmail;
 import static com.cakestation.backend.review.fixture.ReviewFixture.getCreateReviewRequest;
 import static com.cakestation.backend.review.fixture.ReviewFixture.getReviewEntity;
 import static com.cakestation.backend.user.fixture.UserFixture.EMAIL;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mockStatic;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,10 +37,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("ReviewController 는")
 class ReviewControllerTest extends ControllerTest {
 
+    private static MockedStatic<UtilService> utilService;
+
+    @BeforeAll
+    public static void beforeALl() {
+        utilService = mockStatic(UtilService.class);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        utilService.close();
+    }
+
     @DisplayName("리뷰를 등록할 수 있다.")
     @Test
     public void uploadReview() throws Exception {
-        given(utilService.getCurrentUserEmail(anyString())).willReturn(EMAIL);
+        given(getCurrentUserEmail()).willReturn(EMAIL);
         given(reviewService.saveReview(any(CreateReviewDto.class), anyString())).willReturn(1L);
 
         String uri = String.format("/api/stores/%d/reviews", STORE_ID_1);
